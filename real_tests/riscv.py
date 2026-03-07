d94a24745e9429a3cb43cd3f26b64e7bf2fad15a
alexei <aleksey12483@gmail.com>
2026-03-07 15:12:21 +0300

    dm added

--- real_tests/riscv.py
+++ real_tests/riscv.py
@@ -6,22 +6,31 @@ class UARTBehavioralModel:
         self._regs = {i: 0 for i in range(10)}
         
         # Начальное состояние
-        self._regs[3] = 0x03  # LCR: 8 бит
+        self._regs[3] = 0x03  # LCR: WLS=11 (8 бит), DLAB=0
         self._regs[5] = 0x01  # IIR: No interrupt
         self._regs[7] = 0x60  # LSR: THRE=1, TEMT=1
         
-        # Список регистров, в которые ЗАПРЕЩЕНО писать (Status Registers)
         self.READ_ONLY_REGS = {5, 7, 9} # IIR, LSR, MSR
         
         self.current_cycle = 0
         self.memory = []
         self.event_queue = []
 
+    def _get_data_mask(self) -> int:
+        """Определяет маску данных на основе битов LCR[1:0] (WLS)"""
+        wls = self._regs[3] & 0x03
+        masks = {
+            0: 0x1F, # 5 бит
+            1: 0x3F, # 6 бит
+            2: 0x7F, # 7 бит
+            3: 0xFF  # 8 бит
+        }
+        return masks.get(wls, 0xFF)
+
     def _is_fifo_enabled(self) -> bool:
         return bool(self._regs[6] & 0x01)
 
     def _process_hardware(self):
-        """Внутренняя логика перемещения данных"""
         self.event_queue.sort(key=lambda x: x[0])
         remaining_events = []
         
@@ -32,12 +41,10 @@ class UARTBehavioralModel:
                         self.memory.append(data)
                     else:
                         self._regs[0] = data
-                        self._regs[7] |= 0x01 # Set DR (Data Ready)
-                    
-                    self._regs[7] |= 0x60 # Set THRE, TEMT (Empty)
+                        self._regs[7] |= 0x01 # Set DR
+                    self._regs[7] |= 0x60 # Set THRE, TEMT
             else:
                 remaining_events.append((event_cycle, event_type, data))
-        
         self.event_queue = remaining_events
 
     def step_clock(self, cycles: int = 1):
@@ -52,39 +59,37 @@ class UARTBehavioralModel:
 
         if operation == 'read':
             if addr == 0 and not dlab:
-                val = self._regs[0]
-                self._regs[7] &= ~0x01 # Сброс DR при чтении
+                val = self._regs[0] & self._get_data_mask()
+                self._regs[7] &= ~0x01
                 return {'ack': True, 'reg_value': val}
             
-            val = self._regs.get(addr, 0)
-            return {'ack': True, 'reg_value': val & 0xFF}
+            val = self._regs.get(addr, 0) & 0xFF
+            return {'ack': True, 'reg_value': val}
 
         elif operation == 'write':
-            # --- ЗАЩИТА ОТ ЗАПИСИ В REG СОСТОЯНИЯ ---
             if addr in self.READ_ONLY_REGS:
-                # Возвращаем ack=False, так как запись в эти регистры невозможна
-                return {'ack': False, 'reg_value': self._regs[addr]}
+                return {'ack': False, 'reg_value': self._regs[addr] & 0xFF}
             
-            val_to_write = data & 0xFF
-            
-            # Запись в THR (Index 0)
             if addr == 0 and not dlab:
-                self._regs[7] &= ~0x60 # Занято
-                self.event_queue.append((self.current_cycle + 1, "TX_TRANSFER", val_to_write))
-                return {'ack': True, 'reg_value': val_to_write}
-
-            # Запись в остальные разрешенные регистры (LCR, FCR, DLL, DLM, IER, MCR)
-            elif addr in self._regs:
-                # Дополнительная проверка для DLL/DLM (только при DLAB=1)
-                if addr in [1, 2] and not dlab:
-                    return {'ack': True, 'reg_value': self._regs[addr]} # Игнорируем запись
+                # ПРИМЕНЯЕМ МАСКУ WLS ПЕРЕД ПЕРЕДАЧЕЙ
+                mask = self._get_data_mask()
+                val_to_send = data & mask
                 
+                self._regs[7] &= ~0x60
+                self.event_queue.append((self.current_cycle + 1, "TX_TRANSFER", val_to_send))
+                return {'ack': True, 'reg_value': val_to_send}
+
+            elif addr in self._regs:
+                if addr in [1, 2] and not dlab:
+                    return {'ack': True, 'reg_value': self._regs[addr] & 0xFF}
+                
+                val_to_write = data & 0xFF
                 self._regs[addr] = val_to_write
                 return {'ack': True, 'reg_value': val_to_write}
 
         return {'ack': False, 'reg_value': 0}
 
-# Глобальные функции
+# Глобальный API
 _uart_model = UARTBehavioralModel()
 
 def reg_access(addr: int, data: int, operation: str) -> Dict[str, Any]:
